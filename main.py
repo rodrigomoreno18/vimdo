8cb73c0bbdd76906dd430e0f1921c981e69b4562
Rodrigo Moreno <rmoreno.unr@gmail.com>
2021-03-23 01:20:35 -0300

    Basic persistence and some tweaks. Bugs are probably there

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from curses.textpad import Textbox
 
 import curses
 import enum
+import io
+import os
 
 
 class InputStates(enum.Enum):
@@ -12,9 +14,23 @@ class InputStates(enum.Enum):
     TEXT_INPUT = 2
 
 
+class StringEntryData:
+    def __init__(self, data):
+        self._data = data
+
+    def lines(self):
+        return self._data.split("\n")
+
+    def write(self, data):
+        self._data = data
+
+    def append(self, data):
+        self._data += data
+
+
 class ListEntry:
-    def __init__(self, title: str, data: str):
-        self._title = title
+    def __init__(self, title: str, data: StringEntryData):
+        self._title = title.strip()
         self._data = data
 
     @property
@@ -26,9 +42,9 @@ class ListEntry:
         return self._data
 
     def set_title(self, new_title: str):
-        self._title = new_title
+        self._title = new_title.strip()
 
-    def set_data(self, new_data: str):
+    def set_data(self, new_data: StringEntryData):
         self._data = new_data
 
     @classmethod
@@ -36,9 +52,105 @@ class ListEntry:
         return ListEntry(title, data)
 
 
+class EntryBuffer:
+    def __init__(self):
+        self._entries = []
+
+    def __len__(self):
+        return len(self._entries)
+
+    def __iter__(self):
+        return iter(self._entries)
+
+    def __getitem__(self, index):
+        return self._entries[index]
+
+    def append(self, entry: ListEntry):
+        self._entries.append(entry)
+
+    def remove(self, index: int):
+        if 0 <= index < len(self._entries):
+            return self._entries.pop(index)
+
+        raise IndexError("No such entry")
+
+    def entries(self):
+        return self._entries
+
+
+class FilePersistence:
+    def __init__(self, path: str):
+        self._path = path
+
+    def save(self, output_buffer):
+        with open(self._path, "w") as f:
+            f.write(output_buffer.getvalue())
+
+    def load(self):
+        with open(self._path, "r") as f:
+            data = f.readlines()
+            return data
+
+
+class VimdoSerializer:
+
+    TITLE_PREFIX = "### "
+    DATA_PREFIX = "      "
+
+    def __init__(self):
+        pass
+
+    def serialize(self, entry_buffer):
+        output = io.StringIO()
+
+        for entry in entry_buffer.entries():
+            title = f"{self.TITLE_PREFIX}{entry.title}"
+            data = "\n".join(
+                [
+                    f"{self.DATA_PREFIX}{line}"
+                    for line in entry.data.lines()
+                ]
+            )
+
+            serialized_entry = f"{title}\n{data}\n\n"
+            output.write(serialized_entry)
+
+        return output
+
+    def deserialize(self, serialized, entry_buffer):
+        title = None
+        data = []
+        for line in serialized:
+            if line.startswith(self.TITLE_PREFIX):
+                if title is not None:  # Should commit previous entry
+                    entry_buffer.append(
+                        ListEntry(title, StringEntryData("\n".join(data)))
+                    )
+                    title = None
+                    data = []
+
+                title = line[len(self.TITLE_PREFIX):]
+            elif line.startswith(self.DATA_PREFIX):
+                data_line = line[len(self.DATA_PREFIX):].strip('\n')
+                data.append(f"{data_line}")
+        entry_buffer.append(
+            ListEntry(title, StringEntryData("\n".join(data)))
+        )
+
+
 class Vimdo:
-    def __init__(self, stdscr):
+    def __init__(
+        self,
+        stdscr,
+        entry_buffer: EntryBuffer,
+        serializer: VimdoSerializer,
+        storage: FilePersistence,
+    ):
         self._stdscr = stdscr
+        self._buffer = entry_buffer
+        self._serializer = serializer
+        self._storage = storage
+
         self._command_window = curses.newwin(
             1, curses.COLS - 2, curses.LINES - 3, 1
         )
@@ -48,8 +160,6 @@ class Vimdo:
         self._main_window = curses.newwin(
             curses.LINES - 3, curses.COLS - 2, 1, 1
         )
-
-        self._incomplete_entries = []
 
         self._input_state = InputStates.COMMAND
         self._input_number = ""
@@ -80,35 +190,52 @@ class Vimdo:
                     errored = err
 
     def _parse_and_run_command(self, key: str):
-        if key == "q":
+        if key.startswith(r"^["):  # ESC key
+            self._input_number = ""
+        elif key == "q":
             self._input_state = InputStates.EXITING
         elif key == "h":
             self._show_help()
         elif key == "n":
             self._display_command_prompt("Enter note title:")
-            # self._input_state = InputStates.SINGLE_INPUT
             title = self._get_single_line_input()
-            entry = ListEntry.new_entry(title, "")
-            self._incomplete_entries.append(entry)
+            entry = ListEntry.new_entry(
+                title,
+                StringEntryData("Testing data line!\nThis is another line"),
+            )
+            self._buffer.append(entry)
 
         elif key in "0123456789":  # TODO: potentially optimizable
+            if key == "0" and self._input_number == "":
+                return
             self._input_number += key
 
         elif key == "d":
             index = self._parse_and_reset_index()
-            self._incomplete_entries.pop(index)
+            if len(self._buffer) > 0:
+                self._buffer.remove(index)
+            else:
+                raise Exception("No entries to delete!")
 
         elif key == "e":
             index = self._parse_and_reset_index()
-            entry = self._incomplete_entries[index]
+            entry = self._buffer[index]
 
             self._display_command_prompt(f"Editing entry number {index + 1}")
 
-            self._input_window.addstr(0, 0, entry.title)
+            self._input_window.addstr(0, 0, entry.title.strip())
             new_title = self._get_single_line_input()
             entry.set_title(new_title)
+        elif key == "s":
+            self._persist_entries()
+            self._display_command_prompt("List saved!")
+        elif key == "l":
+            self._load_entries()
         else:
-            pass
+            self._input_number = ""
+            raise NotImplementedError(
+                f"Unknown command: {key} - Try 'h' for help"
+            )
 
     def _refresh_windows(self):
         self._main_window.refresh()
@@ -116,7 +243,7 @@ class Vimdo:
         self._input_window.refresh()
 
     def _show_entries(self):
-        for i, entry in enumerate(self._incomplete_entries):
+        for i, entry in enumerate(self._buffer):
             self._main_window.addstr(i, 1, f"{i+1:3} - {entry.title}")
 
     def _show_help(self):
@@ -130,7 +257,7 @@ class Vimdo:
         try:
             index = int(self._input_number)
             self._input_number = ""
-            if index - 1 > len(self._incomplete_entries):
+            if index - 1 > len(self._buffer) > 0:
                 raise IndexError(f"Invalid index! ({index})")
             return index - 1
         except ValueError:
@@ -159,9 +286,21 @@ class Vimdo:
     def _set_state(self, new_state: InputStates):
         self._input_state = new_state
 
+    def _persist_entries(self):
+        serialized = self._serializer.serialize(self._buffer)
+        self._storage.save(serialized)
+
+    def _load_entries(self):
+        self._buffer = EntryBuffer()
+        serialized = self._storage.load()
+        self._serializer.deserialize(serialized, self._buffer)
+
 
 def main(stdscr):
-    vimdo = Vimdo(stdscr)
+    entry_buffer = EntryBuffer()
+    serializer = VimdoSerializer()
+    file_storage = FilePersistence(os.path.join(os.path.curdir, "todo.vimdo"))
+    vimdo = Vimdo(stdscr, entry_buffer, serializer, file_storage)
     vimdo.run()
 
     return
